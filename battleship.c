451c1486392c9ad6da9b51fffb6ede6dd3c0f77e
Lyra Wolves <lyra.wolvespaw@gmail.com>
2019-06-23 02:39:24 -0500

    update

--- battleship.c
+++ battleship.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include "battleship.h"
 
-void setShip(board *play,struct ship *ship){
- for(int y=ship->start_y;y< ship->health;y++)
-  *play[ship->start_x][y]='O';
+void setShips(board *play,struct ship *ship){
+    for
+    for(int y = ship->start_y; y < ship->health ;y++){
+        *play[ship->start_x][y]='O';
+    }
 }
 void fireMissile(board **track1,board **track2,int row, int col){
- board *x=*track1, *y=*track2;
+    board *x=*track1, *y=*track2;
 }
+void printBoard(
 int main(void){
- board player1_play[2];
- board player2_play[2];
+    board player1_play[2];
+    board player2_play[2];
+    struct ship player1[BATTLESHIP_NUMBER];
+    
 }
--- battleship.h
+++ battleship.h
@@ -1,16 +1,17 @@
-#ifndef BATTLESHIP
-#define BATTLESHIP
-//controls board size
-#define BATTLESHIP_BOARD_ROW 10
-#define BATTLESHIP_BOARD_COL 10
-enum SHIP_TYPE{CARRIER,BATTLESHIP,SUPER};//, SUBMARINE, DESTROYER};
-struct ship{
-    enum SHIP_TYPE type;
-    unsigned int health;
-    unsigned int start_x;
-    unsigned int start_y;
-};
-typedef char board [BATTLESHIP_BOARD_ROW][BATTLESHIP_BOARD_COL];
-void setShip(board *,struct ship *);
-void fireMissile(board **,board **,int,int);
+#ifndef BATTLESHIP_H
+    #define BATTLESHIP_H
+    //controls board size
+    #define BATTLESHIP_BOARD_ROW 10
+    #define BATTLESHIP_BOARD_COL 10
+    #define BATTTLESHIP_NUMBER 5
+    enum SHIP_TYPE{CARRIER,BATTLESHIP,SUPER,SUBMARINE,DESTROYER};
+    struct ship{
+        enum SHIP_TYPE type;
+        unsigned int health;
+        unsigned int start_x;
+        unsigned int start_y;
+    };
+    typedef char board [BATTLESHIP_BOARD_ROW][BATTLESHIP_BOARD_COL];
+    void setShip(board *,struct ship *);
+    void fireMissile(board **,board **,int,int);
 #endif
